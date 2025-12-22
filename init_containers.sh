79dbdaf5ece66f5b955f09442a8355ac120e745e
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-12-22 20:26:10 -0300

    detalles de lectura para bash scripts

--- init_containers.sh
+++ init_containers.sh
@@ -3,18 +3,18 @@
 echo -e "\n"
 echo "====================================================================="
 echo "🐳 Iniciando contenedores de infraestructura (Kafka y PostgreSQL)..."
-
+echo "====================================================================="
 # Levanta los servicios Zookeeper, Kafka y Database en modo detached (-d)
 docker compose up -d zookeeper kafka database
 
+echo -e "\n"
+echo "============================================="
 if [ $? -eq 0 ]; then
-    echo -e "\n"
-    echo "============================================="
     echo "✅ Infraestructura iniciada correctamente."
 else
     echo "❌ Error al iniciar la infraestructura Docker. Verifique los logs."
 fi
-
+echo "============================================="
 # El 'sleep' es crucial para darle tiempo a Kafka y PostgreSQL para inicializar antes de conectar las apps Java.
 # echo "⏳ Esperando 15 segundos para inicialización de servicios..."
 # sleep 15
--- init_microservices.sh
+++ init_microservices.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
 echo -e "\n"
-echo "==============================================="
+echo "==============================================================="
 echo "🚀 Iniciando Microservicios de Desarrollo (con DevTools)..."
-echo "==============================================="
+echo "==============================================================="
 # Definir la lista de carpetas de microservicios
 SERVICES=("entrypoint-service" "anomaly-detector-service" "persistence-service" "batch-processing-service")
 
+echo -e "\n"
+echo -e "\n"
+echo "==============================================================="
 for SERVICE in "${SERVICES[@]}"; do
-    echo -e "\n"
-    echo "==============================================="
+
+    echo "==============================================================="
     echo "-> Levantando $SERVICE..."
-    echo "==============================================="
+    echo "==============================================================="
 
     cd $SERVICE
     
     nohup mvn spring-boot:run > $SERVICE.log 2>&1 &
 
-    echo "==============================================="
+    echo "==============================================================="
     echo "-> Service: $SERVICE comenzo correctamente."
-    echo "==============================================="
-    echo -e "\n"
+    echo "==============================================================="
 
     cd ..
 
 done
 
 echo -e "\n"
-echo "==============================================="
+echo -e "\n"
+echo "==============================================================="
 echo "✅ Todos los microservicios están en proceso de inicio."
+echo "==============================================================="
